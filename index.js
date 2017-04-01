0b330d72bd76167c97dea63e4df3734fffe01325
Anwesh43 <anweshthecool0@gmail.com>
2017-04-01 23:27:26 +0530

    adding filter operation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var Observable = Rx.Observable
 Observable.fromEvent(document.getElementById('b1'),'click').subscribe(()=>{
     console.log("clicked")
 })
-Observable.interval(1000).subscribe(()=>{
-    console.log("counting")
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+Observable.interval(1000).map((num)=>numbers[num%10]*10).subscribe((num)=>{
+    console.log(`the output is ${num}`)
 })
